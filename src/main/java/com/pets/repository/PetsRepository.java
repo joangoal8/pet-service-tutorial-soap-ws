0e263a8c501082052e6940a60fef3dd5e8cfafe4
Joan Gomez Alvarez <joan.gomez@edreamsodigeo.com>
2021-02-22 18:56:59 +0100

    adding remove petOwner action

--- src/main/java/com/pets/repository/PetsRepository.java
+++ src/main/java/com/pets/repository/PetsRepository.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Component
 public class PetsRepository {
